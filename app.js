5465cb5259117f21e2821262e5c847f359294732
todsul <todsul@gmail.com>
2015-02-24 22:59:12 -0800

    Add react

--- app.js
+++ app.js
@@ -11,6 +11,8 @@ var users = require('./routes/users');
 
 var app = express();
 
+require('node-jsx').install();
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
 
+var React = require('react/addons');
+var Dashboard = require('../app/components/Dashboard.js');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    var reactHTML = React.renderToString(Dashboard());
+    res.render('index.html', { reactOutput: reactHTML });
 });
 
 module.exports = router;
