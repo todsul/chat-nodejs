8bcf559ab0907dd42288d76c2031e5778294376e
oscarballadares <liebegrube@gmail.com>
2015-02-27 17:05:50 -0600

    config routing

---
+++ config/routing.js
@@ -0,0 +1,48 @@
+var routes = require('../routes/index');
+var users = require('../routes/users');
+var messages = require('../routes/messages');
+var mongoose = require('mongoose');
+var User = mongoose.model('User');
+
+// Mock session user
+var User = mongoose.model('User');
+function getSessionUser(req, res, next) {
+    User.find().limit(1).exec(function(err, users) {
+        var user = users.shift();
+        req.user = user;
+        next();
+    });
+}
+
+function devErrorHandler(err, req, res, next) {
+    res.status(err.status || 500);
+    res.send(err.message);
+}
+
+function prodErrorHandler(err, req, res, next) {
+    res.status(err.status || 500);
+    res.send("Sorry, there was an unexpected error. 500");
+}
+
+function notFoundErrorHandler(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+}
+
+function register(app) {
+    app.use('/', routes);
+    app.use('/users', users);
+    app.use('/messages', messages);
+
+    if (app.get('env') === 'development') {
+        app.use(devErrorHandler);
+    } else {
+        app.use(prodErrorHandler);
+    }
+
+    app.use(notFoundErrorHandler);
+    app.use(getSessionUser);
+}
+
+module.exports = {register: register};
