8ba542658230d261b39870209f65f3964134e29b
oscarballadares <liebegrube@gmail.com>
2015-03-05 16:45:51 -0600

    fixed gulp

--- dist/gulpfile.js
+++ dist/gulpfile.js
@@ -32,7 +32,7 @@ var deployCommands = {
     switchBranch: gitBranch === 'master' ? "echo 'Staying in master branch' ;" : ('cd ' + baseDir + '/releases/flightfox && sudo git checkout --quiet ' + gitBranch + ' ;'),
     echoLastCommit: " cd " + baseDir + "/releases/flightfox && echo \" Branch: `git rev-parse --abbrev-ref HEAD`, last commit: `git log -1 --pretty=oneline --abbrev-commit` \" ",
     npmInstall: 'cd ' + baseDir + '/releases/flightfox && sudo npm install  --loglevel error ;',
-    bundleAssets: 'cd ' + baseDir + '/releases/flightfox && sudo gulp browserify && sudo gulp prod-css && sudo gulp prod-js',
+    bundleAssets: 'cd ' + baseDir + '/releases/flightfox && sudo gulp prod-css && sudo gulp prod-js',
     // @TODO run tests, do other integrity checks
     upgradeToReleaseDir: 'sudo mv ' + baseDir + '/releases/flightfox ' + baseDir + '/releases/' + releaseName + ' ;',
     symlinkNewStartupScript: ' ln -sf ' + baseDir + '/releases/' + releaseName + '/bin/www ' + baseDir + '/www',
