63d5e278e3d2fe9c44ccf101d85b5b9e59141fcc
todsul <todsul@gmail.com>
2015-03-04 17:24:42 -0700

    Update to server.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,35 +1,43 @@
-var browserify = require('browserify');
 var gulp = require('gulp');
+var gutil = require('gutil');
+var browserify = require('browserify');
 var concat = require('gulp-concat');
 var less = require('gulp-less');
-var gutil = require('gutil');
 var reactify = require('reactify');
 var source = require('vinyl-source-stream');
 
 // PATHS
 
-var resourcesDir = './client';
+var clientDir = './client'
+var outputDir = './public'
+
+var browserifySource = clientDir + '/client.js';
+var browserifyOutput = outputDir + '/js/';
+var browserifyFile = 'scripts.js';
+
+var lesscssSource = clientDir + '/styles/styles.less';
+var lesscssOutput = outputDir + '/css/';
 
 // DEV TASKS
 
-gulp.task('less', function () {
-    gulp.src('client/styles/styles.less')
-        .pipe(less().on('error', gutil.log))
-        .pipe(gulp.dest('public/css'))
-});
-
-gulp.task('react', function () {
-    return browserify('./client/client.js')
+gulp.task('browserify', function () {
+    return browserify(browserifySource)
         .transform(reactify)
         .on('error', gutil.log)
         .bundle()
-        .pipe(source('scripts.js'))
-        .pipe(gulp.dest('./public/js/'));
+        .pipe(source(browserifyFile))
+        .pipe(gulp.dest(browserifyOutput));
+});
+
+gulp.task('lesscss', function () {
+    gulp.src(lesscssSource)
+        .pipe(less().on('error', gutil.log))
+        .pipe(gulp.dest(lesscssOutput))
 });
 
 gulp.task('watch', function() {
-    gulp.watch('./client', ['less']);
-    gulp.watch('./client', ['react']);
+    gulp.watch(clientDir, ['browserify']);
+    gulp.watch(clientDir, ['lesscss']);
 });
 
 // PROD TASKS
